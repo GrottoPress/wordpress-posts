cd5b8ef5c2e34d1502421e3fbae6d25019688279
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2017-10-07 13:22:00 +0000

    converted more of gulpfile.js to es6 syntax

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,47 @@
 /**
  * Gulpfile
  *
- * @see https://travismaynard.com/writing/getting-started-with-gulp
- *
  * @since 0.1.0
  */
 
-'use strict';
+'use strict'
 
 // Include gulp
-const gulp = require('gulp');
+const gulp = require('gulp')
 
-// Include Our Plugins
-const rename = require('gulp-rename');
-const rtlcss = require('gulp-rtlcss');
-const cleanCSS = require('gulp-clean-css');
-const sass = require('gulp-sass');
+// Include plugins
+const rename = require('gulp-rename')
+const rtlcss = require('gulp-rtlcss')
+const cleanCSS = require('gulp-clean-css')
+const sass = require('gulp-sass')
 
-// Files/Paths
-const sass_files = ['./src/assets/styles/**/*.scss'];
-const sass_dest = './dist/assets/styles';
+// Paths
+const sass_files = ['./src/assets/styles/**/*.scss']
+const sass_dest = './dist/assets/styles'
 
 // Compile scss, rtl and minify scss
-gulp.task('compile_sass', function () {
-    return gulp.src(sass_files)
-        .pipe(sass().on('error', sass.logError))
-        // .pipe(cleanCSS({format: 'beautify'}))
-        .pipe(gulp.dest(sass_dest))
-        .pipe(cleanCSS())
-        .pipe(rename({'suffix' : '.min'}))
-        .pipe(gulp.dest(sass_dest))
-        .pipe(rtlcss())
-        .pipe(rename(function (path) {
-            path.basename = path.basename.replace('.min', '-rtl.min');
-        }))
-        .pipe(gulp.dest(sass_dest));
-});
+gulp.task('compile_sass', () =>
+    gulp.src(sass_files)
+    .pipe(sass().on('error', sass.logError))
+    // .pipe(cleanCSS({format: 'beautify'}))
+    .pipe(gulp.dest(sass_dest))
+    .pipe(cleanCSS())
+    .pipe(rename({'suffix' : '.min'}))
+    .pipe(gulp.dest(sass_dest))
+    .pipe(rtlcss())
+    .pipe(rename((path) =>
+        path.basename = path.basename.replace('.min', '-rtl.min')
+    ))
+    .pipe(gulp.dest(sass_dest))
+)
 
-// Watch Files For Changes
-gulp.task('watch', function () {
-    gulp.watch(sass_files, ['compile_sass']);
-});
+// Watchfiles for changes
+gulp.task('watch', () =>
+    gulp.watch(sass_files, ['compile_sass'])
+)
 
-// Default Task
+// Default task
 gulp.task('default', [
     'compile_sass',
     'watch'
-]);
+])

3716530cbb7fa08ab6df7d60930836a19185e540
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2018-03-10 17:07:35 +0000

    Update doc block comment

--- gulpfile.js
+++ gulpfile.js
@@ -24,23 +24,23 @@ const styles_dest = './dist/styles'
 const vendor_assets = './assets/vendor'
 
 /**
- * Compile scss, rtl, minify css
+ * Build styles
  */
 gulp.task('styles', () =>
     gulp.src(styles_src)
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    // .pipe(cleanCSS({format: 'beautify'}))
-    // .pipe(gulp.dest(styles_dest))
-    .pipe(cleanCSS())
-    .pipe(rename({'suffix' : '.min'}))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(styles_dest))
-    .pipe(rtlcss())
-    .pipe(rename((path) =>
-        path.basename = path.basename.replace('.min', '-rtl.min')
-    ))
-    .pipe(gulp.dest(styles_dest))
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        // .pipe(cleanCSS({format: 'beautify'}))
+        // .pipe(gulp.dest(styles_dest))
+        .pipe(cleanCSS())
+        .pipe(rename({'suffix' : '.min'}))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(styles_dest))
+        .pipe(rtlcss())
+        .pipe(rename((path) =>
+            path.basename = path.basename.replace('.min', '-rtl.min')
+        ))
+        .pipe(gulp.dest(styles_dest))
 )
 
 /**
@@ -48,7 +48,7 @@ gulp.task('styles', () =>
  */
 gulp.task('vendor', () => {
     gulp.src(['./node_modules/@grottopress/scss/**'])
-    .pipe(gulp.dest(`${vendor_assets}/@grottopress/scss`))
+        .pipe(gulp.dest(`${vendor_assets}/@grottopress/scss`))
 })
 
 /**

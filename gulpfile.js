a9fcf39ade0d69a03fecc9c7e211cc89e0184db1
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2018-03-20 15:13:14 +0000

    Remove redundant doc blocks and comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,5 @@
-/**
- * Gulpfile
- *
- * @since 0.1.0
- */
-
 'use strict'
 
-/**
- * Import gulp, plugins
- */
 const gulp = require('gulp')
 const rename = require('gulp-rename')
 const rtlcss = require('gulp-rtlcss')
@@ -16,16 +7,10 @@ const cleanCSS = require('gulp-clean-css')
 const sass = require('gulp-sass')
 const sourcemaps = require('gulp-sourcemaps')
 
-/**
- * Define paths
- */
 const styles_src = ['./assets/styles/**/*.scss']
 const styles_dest = './dist/styles'
 const vendor_assets = './assets/vendor'
 
-/**
- * Build styles
- */
 gulp.task('styles', () =>
     gulp.src(styles_src)
         .pipe(sourcemaps.init())
@@ -43,24 +28,15 @@ gulp.task('styles', () =>
         .pipe(gulp.dest(styles_dest))
 )
 
-/**
- * Build vendor assets
- */
 gulp.task('vendor', () => {
     gulp.src(['./node_modules/@grottopress/scss/**'])
         .pipe(gulp.dest(`${vendor_assets}/@grottopress/scss`))
 })
 
-/**
- * Watch files for changes
- */
 gulp.task('watch', () =>
     gulp.watch(styles_src, ['styles'])
 )
 
-/**
- * Default task
- */
 gulp.task('default', [
     'styles',
     'watch'

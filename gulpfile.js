972a5e84c134f62d8ff7cd41e9557a62c7075f62
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2018-07-27 17:19:49 +0000

    Set up [postcss](https://postcss.org/)
    Using postcss with cssnano and css-mqpacker

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,11 @@
 const gulp = require('gulp')
 const rename = require('gulp-rename')
 const rtlcss = require('gulp-rtlcss')
-const cleanCSS = require('gulp-clean-css')
 const sass = require('gulp-sass')
 const sourcemaps = require('gulp-sourcemaps')
+const postcss = require('gulp-postcss')
+const cssnano = require('cssnano')
+const mqpacker = require('css-mqpacker')
 
 const styles_src = ['./assets/styles/**/*.scss']
 const styles_dest = './dist/styles'
@@ -15,9 +17,7 @@ gulp.task('styles', () =>
     gulp.src(styles_src)
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
-        // .pipe(cleanCSS({format: 'beautify'}))
-        // .pipe(gulp.dest(styles_dest))
-        .pipe(cleanCSS())
+        .pipe(postcss([cssnano(), mqpacker()]))
         .pipe(rename({'suffix' : '.min'}))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(styles_dest))
